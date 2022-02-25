59c92c58e1b3025103ffb32c2b5a5efa9c5baf89
IvanS99 <ivan.samardzic41@gmail.com>
2022-02-25 13:41:08 +0100

    Initial commit

---
+++ src/main/java/org/unib/etf/epraksa/EPraksaApplication.java
@@ -0,0 +1,13 @@
+package org.unib.etf.epraksa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EPraksaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EPraksaApplication.class, args);
+    }
+
+}
