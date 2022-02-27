ec3f324eac2b1d03fe7fe6e50d095aa50efc5e68
IvanS99 <ivan.samardzic41@gmail.com>
2022-02-27 17:44:13 +0100

    sitan fix

--- src/main/java/org/unib/etf/epraksa/config/ModelMapperConfig.java
+++ src/main/java/org/unib/etf/epraksa/config/ModelMapperConfig.java
@@ -3,7 +3,9 @@ package org.unib.etf.epraksa.config;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ModelMapperConfig {
     @Bean
     public ModelMapper getModelMapper()
