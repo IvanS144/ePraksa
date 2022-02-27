3acfc472ef9f66198e3b54901225183ffe83b209
IvanS99 <ivan.samardzic41@gmail.com>
2022-02-27 17:23:27 +0100

    Logovanje u fajl

--- src/main/java/org/unib/etf/epraksa/advices/GlobalExceptionHandler.java
+++ src/main/java/org/unib/etf/epraksa/advices/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package org.unib.etf.epraksa.advices;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
@@ -7,11 +9,13 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.method.HandlerMethod;
 import org.unib.etf.epraksa.exceptions.HttpException;
+import org.unib.etf.epraksa.util.LoggingUtil;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(HttpException.class)
-    public final ResponseEntity<Object> handleHttpException(HttpException e) {
+    public final ResponseEntity<Object> handleHttpException(HttpException e, HandlerMethod handlerMethod) {
+        LoggingUtil.logException(e, getLog(handlerMethod));
         if (e.getStatus() == null)
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         return new ResponseEntity<>(e.getData(), e.getStatus());
@@ -23,6 +27,7 @@ public class GlobalExceptionHandler {
     }
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public final ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException e, HandlerMethod handlerMethod) {
+        LoggingUtil.logException(e, getLog(handlerMethod));
         return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
     }
     @ExceptionHandler(org.hibernate.StaleObjectStateException.class)
@@ -31,7 +36,12 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>("U međuvremenu je došlo do izmjene, provjerite podatke pa pokušajte ponovo", HttpStatus.CONFLICT);
     }
     @ExceptionHandler(Exception.class)
-    public final ResponseEntity<Object> handleException(Exception e){
+    public final ResponseEntity<Object> handleException(Exception e, HandlerMethod handlerMethod){
+        LoggingUtil.logException(e, getLog(handlerMethod));
         return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    private Log getLog(HandlerMethod handlerMethod) {
+        return LogFactory.getLog(handlerMethod.getMethod().getDeclaringClass());
     }
 }
