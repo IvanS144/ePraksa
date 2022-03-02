4462cbac087581f2115e51aecb9263cdbcb2a217
IvanS99 <ivan.samardzic41@gmail.com>
2022-03-02 21:58:32 +0100

    dodao kontroler i endpoint za login

---
+++ src/main/java/org/unibl/etf/epraksa/controllers/AuthController.java
@@ -0,0 +1,26 @@
+package org.unibl.etf.epraksa.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.unibl.etf.epraksa.model.requests.LoginRequest;
+import org.unibl.etf.epraksa.services.AuthService;
+
+import javax.validation.Valid;
+
+@RestController
+public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("api/v1/authLogin")
+    public ResponseEntity<Object> login(@RequestBody @Valid LoginRequest request)
+    {
+        return authService.login(request);
+    }
+}
