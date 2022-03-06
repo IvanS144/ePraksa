9342f1b10629a37f36dcfaf792d286a62daec7a0
Milos Jovicic <jovicicm55@outlook.com>
2022-03-06 19:52:39 +0100

    input -> entry, imena klasa

--- src/main/java/org/unibl/etf/epraksa/model/entities/json/OneEntryForQuestionnaireJSON.java
+++ src/main/java/org/unibl/etf/epraksa/model/entities/json/OneEntryForQuestionnaireJSON.java
@@ -3,7 +3,7 @@ package org.unibl.etf.epraksa.model.entities.json;
 import lombok.Data;
 
 @Data
-public class OneInputForQuestionnaireJSON {
+public class OneEntryForQuestionnaireJSON {
     private Long id;
     private String question;
     private AnswerToTheQuestionnaireENUM answer;
--- src/main/java/org/unibl/etf/epraksa/model/entities/json/OneEntryWorkDiaryJSON.java
+++ src/main/java/org/unibl/etf/epraksa/model/entities/json/OneEntryWorkDiaryJSON.java
@@ -3,7 +3,7 @@ package org.unibl.etf.epraksa.model.entities.json;
 import lombok.Data;
 
 @Data
-public class OneInputWorkDiaryJSON {
+public class OneEntryWorkDiaryJSON {
     private Long id;
     private String date;
     private String day;
