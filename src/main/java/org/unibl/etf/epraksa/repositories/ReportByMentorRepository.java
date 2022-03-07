1a7762097c6166959c5f1b67589115a9c9e6fc9a
Milos Jovicic <jovicicm55@outlook.com>
2022-03-07 09:49:18 +0100

    popravio query

--- src/main/java/org/unibl/etf/epraksa/repositories/ReportByMentorRepository.java
+++ src/main/java/org/unibl/etf/epraksa/repositories/ReportByMentorRepository.java
@@ -5,10 +5,9 @@ import org.springframework.data.jpa.repository.Query;
 import org.unibl.etf.epraksa.model.entities.ReportByMentor;
 
 public interface ReportByMentorRepository extends JpaRepository<ReportByMentor, Long> {
-    @Query(value = "select rbm from ReportByMentor rbm " +
-            "where exists (" +
-            "select shi from StudentHasInternship shi " +
-            "where shi.id.id = :id and shi.id.internshipId = :internshipId" +
-            ")")
+    @Query(value = "select rbm.reportId, rbm.opinionJSON, rbm.gradingJSON, rbm.createdAt, rbm.lastModifiedDate, rbm.deletedDate " +
+            "from ReportByMentor rbm " +
+            "inner join StudentHasInternship shi on shi.report.reportId = rbm.reportId " +
+            "where shi.id.id = :id and shi.id.internshipId = :internshipId")
     ReportByMentor getReport(Long id, Long internshipId);
 }
