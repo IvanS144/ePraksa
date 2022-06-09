0cbe7f64b12bfb0e095caee7cf0ed2bf06ec84dd
IvanS99 <ivan.samardzic41@gmail.com>
2022-06-09 11:31:49 +0200

    auditing

--- src/main/java/org/unibl/etf/epraksa/model/entities/Notification.java
+++ src/main/java/org/unibl/etf/epraksa/model/entities/Notification.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.time.LocalDate;
@@ -13,6 +14,7 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 @Table(name="notification")
 public class Notification {
     @Id
